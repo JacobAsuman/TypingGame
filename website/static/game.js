ec0fcea24f8b197d6124659ead3f6a0dbffe9e34
JacobAsuman <75538352+JacobAsuman@users.noreply.github.com>
2023-03-20 01:27:41 +0000

    Calculate words per minute

--- website/static/game.js
+++ website/static/game.js
@@ -1,9 +1,13 @@
 window.addEventListener('load', init);
 //Global vars
 let accuracy= 100;
+let mistakes=0;
 let wpm = 0;
 let isPlaying = false;
 let cDown = 5;
+let words = 0;
+let start;
+let now;
 
 //DOM elements
 const timeIndicator = document.querySelector('#count-down');
@@ -41,37 +45,46 @@ function showPrompt(prompts){
     chosenPhrase.innerHTML = prompts[randIndex];
     let wordArr = prompts[randIndex].split(" ");
     wordInput.readOnly = false;
+    start = Date.now();
     //console.log("working so far...");
     //console.log(wordArr);
     //console.log("TESTing" + toType.substring(0, 1));
     isPlaying = true;
     wordInput.addEventListener('input', letterMatch);
+    //accDisplay.addEventListener('input', calcAccuracy)
 }
 
 function letterMatch(){
     if(isMatching()){
-        console.log("Match");
+        //console.log("Match");
     }
     if(isCompleted()){
         wordInput.readOnly = true;
         message.innerHTML = 'Finished!';
         console.log("Finished");
     }
+
+    calcAccuracy();
+    calcWPM();
     
 }
 
 function isMatching(){ 
     var text = document.getElementById('word-input').value; // Letters being typed
     var prompt = chosenPhrase.innerHTML;    // Letters needed to be typed
-    console.log(prompt[text.length-1]);
-    console.log(text[text.length-1]);
-    console.log(chosenPhrase.innerHTML);
-    console.log(text.length);
-    console.log(prompt.length);
+    //console.log(prompt[text.length-1]);
+    //console.log(text[text.length-1]);
+    //console.log(chosenPhrase.innerHTML);
+    //console.log(text.length);
+    //console.log(prompt.length);
     if(prompt[text.length-1] === text[text.length-1]){  //Compare what's typed to what needs to be typed
         message.innerHTML = 'Correct!';
         wordInput.style.borderWidth='thick';
         wordInput.style.borderColor='green';
+        if(text[text.length-1] === " "){
+            words +=1;
+            console.log(words);
+        }
         //chosenPhrase.style.backgroundColor='green';
         //chosenPhrase.innerHTML = chosenPhrase.textContent.replace(prompt[text.length-1], match =>`<mark>${match[text.length-1]}</mark>`);
         //chosenPhrase.innerHTML = chosenPhrase.textContent.replace(chosenPhrase.innerHTML,"<span style='background-color: #FFFF00'>" + prompt.substring(0, text.length-1) + "</span>" + prompt.substring(text.length, prompt.length));
@@ -79,6 +92,7 @@ function isMatching(){
     }else{
         message.innerHTML = '';
         wordInput.style.borderColor='red';
+        mistakes += 1;
         return false;
     }
 }
@@ -111,4 +125,20 @@ function checkPlaying(){
     if(!isPlaying){
         message.innerHTML = 'Game Over.';
     }
+}
+
+function calcAccuracy(){
+    var text = document.getElementById('word-input').value; // Letters being typed
+    accuracy = ((text.length-mistakes)/text.length)*100;
+    accuracy = accuracy.toFixed(2);
+    accDisplay.innerHTML = accuracy;
+    //console.log(accuracy);
+}
+
+function calcWPM(){
+    now = Date.now();
+    let elapsed = (now-start)/1000;
+    wpm = (words/elapsed)*60;
+    wpmDisplay.innerHTML = wpm;
+    console.log(wpm);
 }
